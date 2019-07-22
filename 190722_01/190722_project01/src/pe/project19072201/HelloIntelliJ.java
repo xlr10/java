c0f1e78491de0ae42c90827558adcaec3cc4b018
xlr10 <vjihovjiho@gmail.com>
2019-07-22 22:44:32 +0900

    모듈간 import

--- 190722_01/190722_project01/src/pe/project19072201/HelloIntelliJ.java
+++ 190722_01/190722_project01/src/pe/project19072201/HelloIntelliJ.java
@@ -1,7 +1,9 @@
 package pe.project19072201;
 
 public class HelloIntelliJ {
-    public static void main(String[] args) {
-        System.out.println("asdasd");
+    //인스턴스에서 바로 메소드 호출하는거 기억안나네. 익명클래스인가
+    public void importEx(){
+        System.out.println("This is project19072201");
+        System.out.println("I'm Imported");
     }
 }
