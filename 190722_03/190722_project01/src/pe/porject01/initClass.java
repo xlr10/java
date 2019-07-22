d8d637db0508f4e38926d6f75d2c217df96c78c3
xlr10 <vjihovjiho@gmail.com>
2019-07-22 21:50:45 +0900

    190722_03/ --push test03--

---
+++ 190722_03/190722_project01/src/pe/porject01/initClass.java
@@ -0,0 +1,12 @@
+package pe.porject01;
+
+import org.apache.commons.lang.StringUtils;
+
+public class initClass {
+    public static void main(String[] args) {
+        System.out.println("asdasd");
+        String name = null;
+        System.out.println(StringUtils.isEmpty(name));
+        System.out.println("asdasd3");
+    }
+}
